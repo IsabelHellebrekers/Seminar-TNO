04b1936de7a80c2673dc1fbca71069d45a46523f
Ies Timmerarends <id@ilgv.net>
2026-02-06 11:58:51 +0100

    Corrected the dispersed concept, and added gitignore for gurobi

--- src/DataUtils/InstanceCreator.java
+++ src/DataUtils/InstanceCreator.java
@@ -15,14 +15,14 @@ public class InstanceCreator {
         List<OperatingUnit> operatingUnits = new ArrayList<>();
         operatingUnits.add(new OperatingUnit("VUST", "VUST", 13018, 42842, 67140, 39054, 128526, 201420, "MSC"));
         operatingUnits.add(new OperatingUnit("GN_CIE_1", "GN_CIE", 6209, 21072, 43718, 18627, 63216, 131154, "FSC_1"));
-        operatingUnits.add(new OperatingUnit("GN_CIE_2", "GN_CIE", 6966, 24558, 39476, 20898, 73674, 118428, "FSC_2"));
         operatingUnits.add(new OperatingUnit("PAINF_CIE_1", "PAINF_CIE", 8086, 31693, 58222, 24258, 95079, 174666, "FSC_1"));
         operatingUnits.add(new OperatingUnit("PAINF_CIE_2", "PAINF_CIE", 11197, 26127, 60676, 33591, 78381, 182028, "FSC_1"));
         operatingUnits.add(new OperatingUnit("PAINF_CIE_3", "PAINF_CIE", 9447, 31049, 57504, 28341, 93147, 172512, "FSC_1"));
-        operatingUnits.add(new OperatingUnit("PAINF_CIE_4", "PAINF_CIE", 11570, 33486, 52944, 34710, 100458, 158832, "FSC_2"));
-        operatingUnits.add(new OperatingUnit("PAINF_CIE_5", "PAINF_CIE", 10351, 31801, 55848, 31053, 95403, 167544, "FSC_2"));
         operatingUnits.add(new OperatingUnit("AT_CIE_1", "AT_CIE", 3476, 7921, 17603, 10428, 23763, 52809, "FSC_1"));
         operatingUnits.add(new OperatingUnit("AT_CIE_2", "AT_CIE", 2484, 7898, 18618, 7452, 23694, 55854, "FSC_1"));
+        operatingUnits.add(new OperatingUnit("GN_CIE_2", "GN_CIE", 6966, 24558, 39476, 20898, 73674, 118428, "FSC_2"));
+        operatingUnits.add(new OperatingUnit("PAINF_CIE_4", "PAINF_CIE", 11570, 33486, 52944, 34710, 100458, 158832, "FSC_2"));
+        operatingUnits.add(new OperatingUnit("PAINF_CIE_5", "PAINF_CIE", 10351, 31801, 55848, 31053, 95403, 167544, "FSC_2"));
         operatingUnits.add(new OperatingUnit("AT_CIE_3", "AT_CIE", 3033, 9628, 16339, 9099, 28884, 49017, "FSC_2"));
 
         List<FSC> fscs = new ArrayList<>();
@@ -51,10 +51,9 @@ public class InstanceCreator {
         Sampling sampler = new Sampling();
         List<OperatingUnit> operatingUnits = new ArrayList<>();
 
-        operatingUnits.add(new OperatingUnit("VUST","VUST",sampler.stochasticFW(13018),sampler.stochasticFUEL(42842),sampler.stochasticAMMO(67140),39054,128526,201420,"MSC"));
         operatingUnits.add(new OperatingUnit("GN_CIE_1","GN_CIE",sampler.stochasticFW(6209),sampler.stochasticFUEL(21072),sampler.stochasticAMMO(43718),18627,63216,131154,"FSC_1"));
-        operatingUnits.add(new OperatingUnit("GN_CIE_2","GN_CIE",sampler.stochasticFW(6966),sampler.stochasticFUEL(24558),sampler.stochasticAMMO(39476),20898,73674,118428,"FSC_2"));
         operatingUnits.add(new OperatingUnit("PAINF_CIE_1","PAINF_CIE",sampler.stochasticFW(8086),sampler.stochasticFUEL(31693),sampler.stochasticAMMO(58222),24258,95079,174666,"FSC_1"));
+        operatingUnits.add(new OperatingUnit("GN_CIE_2","GN_CIE",sampler.stochasticFW(6966),sampler.stochasticFUEL(24558),sampler.stochasticAMMO(39476),20898,73674,118428,"FSC_2"));
         operatingUnits.add(new OperatingUnit("PAINF_CIE_2","PAINF_CIE",sampler.stochasticFW(11197),sampler.stochasticFUEL(26127),sampler.stochasticAMMO(60676),33591,78381,182028,"FSC_1"));
         operatingUnits.add(new OperatingUnit("PAINF_CIE_3","PAINF_CIE",sampler.stochasticFW(9447),sampler.stochasticFUEL(31049),sampler.stochasticAMMO(57504),28341,93147,172512,"FSC_1"));
         operatingUnits.add(new OperatingUnit("PAINF_CIE_4","PAINF_CIE",sampler.stochasticFW(11570),sampler.stochasticFUEL(33486),sampler.stochasticAMMO(52944),34710,100458,158832,"FSC_2"));
