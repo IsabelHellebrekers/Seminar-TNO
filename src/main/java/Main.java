a482d54b56f5135b97a95c9d8a0e2c19d60eb848
Isabel Hellebrekers <612348ih@eur.nl>
2026-02-17 14:27:43 +0100

    Moved solution files to 'results' folder. Deleted old solutions.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,8 +17,8 @@ public class Main {
 //        List<Result> results = CapacitatedResupplyMILP.solveInstances(new InstanceCreator().createFDInstance());
 
     // Solve Dispersed Concept (Deterministic MILP for contiguous partitions)
-       List<Result> results = CapacitatedResupplyMILP.solveInstances(new InstanceCreator().contiguousPartitions());
-       new OutputCreator().createCSV(results);
+    //    List<Result> results = CapacitatedResupplyMILP.solveInstances(new InstanceCreator().contiguousPartitions());
+    //    new OutputCreator().createCSV(results);
 
         // Out of Sample testing
         // OutOfSampleMILP.solveInstances(InstanceCreator.createFDInstance(), 200.0, Map.of("FSC_1", 50.0, "FSC_2", 50.0));
